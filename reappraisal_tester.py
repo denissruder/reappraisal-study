bbc413b901e67c320e317170e4cf13a27b490302
denissruder <deniss.ruder@gmail.com>
2025-12-12 10:48:10 +0200

    Update reappraisal_tester.py

--- reappraisal_tester.py
+++ reappraisal_tester.py
@@ -671,8 +671,7 @@ def show_motives_only_page():
         motive_scores = st.session_state.general_motive_scores
         for m in MOTIVES_FULL:
             st.markdown("<hr style='margin: 5px 0 15px 0; border: 0.5px solid #eee;'>", unsafe_allow_html=True)
-            # st.markdown(f"**{m['motive']}** - {m['Definition']}")
-            st.markdown(f"<p style='font-size: 0.9rem; margin-bottom: 0.5px;'><b>{m['motive']}</b> - {m['Definition']}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size: 0.9rem; margin-bottom: 10px;'><b>{m['motive']}</b> - {m['Definition']}</p>", unsafe_allow_html=True)
             
             # Create two equally sized columns inside the form
             col1, col2 = st.columns(2) 
