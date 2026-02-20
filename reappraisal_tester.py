05f583cc88c02891757e56ba69cdbd8f7f488904
denissruder <deniss.ruder@gmail.com>
2026-02-20 09:38:09 +0200

    Update reappraisal_tester.py

--- reappraisal_tester.py
+++ reappraisal_tester.py
@@ -259,7 +259,7 @@ def show_motives():
     val = st.session_state.event_order[idx]
     st.header(f"Phase 2: Motive Ratings ({val} Event)")
     
-    st.info(f"Please rate the importance of the following motives to you based on the event you described.)
+    st.info(f"Please rate the importance of the following motives to you based on the event you described.")
 
     with st.expander("Reference: Your Narrative", expanded=True):
         st.write(st.session_state[f"final_narrative_{idx}"])
